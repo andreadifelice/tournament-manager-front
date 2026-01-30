e3f72e10dda674023c64d4e13f6928f2fa278185
adf_creations <andreadifelice54@gmail.com>
2026-01-30 15:45:38 +0100

    modifiche a main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import HomePage from './pages/HomePage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <HomePage/>
   </StrictMode>,
 )
