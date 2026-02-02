2fb41b1f994d6aa994388e03553746fd682123a8
adf_creations <andreadifelice54@gmail.com>
2026-02-02 11:40:11 +0100

    .

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,20 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
-
-export function sleep(time: number) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-        resolve(undefined)
-        }, time);
-    })
-}
-
-export function download(dataUrl: string, fileName = 'map-architect') {
-    const a = document.createElement('a');
-    a.setAttribute('download', fileName);
-    a.setAttribute('href', dataUrl);
-    a.click();
-}
-
-//export async function fileToDataUrl() 
