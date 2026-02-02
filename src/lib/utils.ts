edc1c1ade99016516d0a18f4982c4d17f457c249
adf_creations <andreadifelice54@gmail.com>
2026-02-02 11:37:43 +0100

    continuo del setup frontend

---
+++ src/lib/utils.ts
@@ -0,0 +1,23 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+export function sleep(time: number) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+        resolve(undefined)
+        }, time);
+    })
+}
+
+export function download(dataUrl: string, fileName = 'map-architect') {
+    const a = document.createElement('a');
+    a.setAttribute('download', fileName);
+    a.setAttribute('href', dataUrl);
+    a.click();
+}
+
+//export async function fileToDataUrl() 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import HomePage from './pages/HomePage'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    //element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }
+    ]
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HomePage/>
+    <RouterProvider router={router}/>
+    
   </StrictMode>,
 )
