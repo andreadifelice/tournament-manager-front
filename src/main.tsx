93e4133a121432eb45f2e3b4780d4dfe59f18c66
adf_creations <andreadifelice54@gmail.com>
2026-02-02 11:47:53 +0100

    collegamento rotte

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 import HomePage from './pages/HomePage'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 
+
+// creo una costante per poter navigare con le routes tra le pagine che vado a creare
 const router = createBrowserRouter([
   {
     path: '/',
@@ -20,6 +22,5 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router}/>
-    
   </StrictMode>,
 )
